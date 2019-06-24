412c12c3aacdddd81ee7fad419d36fc4d9552f2f
Kingsley Silas Chijioke <kinsomicrote.inc@gmail.com>
2019-06-24 08:59:25 +0100

    Updated Todo component

--- src/Todo.js
+++ src/Todo.js
@@ -1,3 +1,4 @@
+
 import React, { useRef } from "react";
 import {useImmerReducer } from "use-immer";
 
@@ -7,27 +8,26 @@ const reducer = (draft, action) => {
 
   switch (action.type) {
     case "ADD_ITEM":
-      draft.push(action.payload);
+      draft.push(action.item);
       return;
     case "clear":
       return initialState;
     default:
-      throw new Error();
+      return draft;
   }
 }
 
 const Todo = () => {
   const inputEl = useRef(null);
   const [state, dispatch] = useImmerReducer(reducer, initialState);
-  console.log('state', state)
   
   const handleSubmit = (e) => {
     e.preventDefault()
-    const todoobj = {
+    const newItem = {
       text: inputEl.current.value,
       done: false
     };
-    dispatch({ type: "ADD_ITEM", payload: todoobj });
+    dispatch({ type: "ADD_ITEM", item: newItem });
 
     inputEl.current.value = "";
     inputEl.current.focus();
